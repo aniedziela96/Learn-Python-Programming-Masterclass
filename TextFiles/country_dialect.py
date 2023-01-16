31df4131e3a3c3a1de78da80b4cab465d911f547
aniedziela96 <niedziela.aleksandra@gmail.com>
2023-01-16 23:54:59 +0100

    16.01 - afternoon study

--- TextFiles/country_dialect.py
+++ TextFiles/country_dialect.py
@@ -7,7 +7,21 @@ with open(input_filename, encoding='utf-8', newline='') as countries_data:
     for line in range(3):
         sample += countries_data.read()
     country_dialect = csv.Sniffer().sniff(sample)
+    country_dialect.skipinitialspace = True
     countries_data.seek(0)
     country_reader = csv.reader(countries_data, dialect=country_dialect)
     for row in country_reader:
         print(row)
+
+print('*' * 80)
+atribiutes = ['delimiter',
+              'doublequote',
+              'escapechar',
+              'lineterminator',
+              'quotechar',
+              'quoting',
+              'skipinitialspace'
+              ]
+
+for atribiute in atribiutes:
+    print(f'{atribiute}: {repr(getattr(country_dialect, atribiute))}')
